ba32e573833aaa2e82dc6c30d07a798e4566a7bf
Mauro Luyten <mauro.luyten@gmail.com>
2017-10-24 10:56:39 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,59 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    Button,
+    ToolbarAndroid,
+    View,
+    StatusBar
+} from 'react-native';
+import {StackNavigator} from 'react-navigation'
+import HomeScreen from './Components/HomeScreen'
+import ProfileScreen from './Components/ProfileScreen'
+
+const Application = StackNavigator({
+    Home: {
+        screen: HomeScreen
+    },
+    Profile: {
+        screen: ProfileScreen
+    }
+})
+
+export default class App extends Component {
+    render() {
+        return (
+            < Application />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5
+    },
+    toolbar: {
+        height: 60,
+        backgroundColor: 'blue'
+    }
+});
---
+++ Components/HomeScreen.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import {View, Text, Button} from 'react-native'
+import {StackNavigator} from 'react-navigation';
+export default class HomeScreen extends Component {
+    static navigationOptions = {
+        title: 'Home'
+    }
+    render(){
+        const {navigate} = this.props.navigation
+        return (
+            <View>
+                <Text>
+                    Test
+                </Text>
+                <Button
+                    title="Profile"
+                    onPress={()=>navigate('Profile')}
+                />
+            </View>
+        )
+    }
+}
